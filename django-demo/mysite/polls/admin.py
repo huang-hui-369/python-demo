56347cf61ffe68a88f2d42df921a61622d99565e
huang-hui <huanghui@grandunit.net>
2021-05-11 12:41:41 +0900

    admin.pyを改修、choice　→　ChoiceInline

--- django-demo/mysite/polls/admin.py
+++ django-demo/mysite/polls/admin.py
@@ -4,6 +4,21 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+# admin.site.register(Question)
 
-admin.site.register(Choice)
+# admin.site.register(Choice)
+
+# StackedInline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
