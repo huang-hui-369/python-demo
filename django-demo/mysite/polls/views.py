8c019db42e4a4c6c21724a182630276824be9beb
huang-hui <huanghui@grandunit.net>
2021-05-11 12:26:31 +0900

    汎用ビューの利用

--- django-demo/mysite/polls/views.py
+++ django-demo/mysite/polls/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 
 from django.template import loader
 
+from django.views import generic
+
 from .models import Question, Choice
 
 
@@ -20,12 +22,23 @@ from .models import Question, Choice
 #     }
 #     return HttpResponse(template.render(context, request))
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+
+# ListView 汎用ビューは <app name>/<model name>_list.html というデフォルトのテンプレートを使う
+# ListView では、自動的に生成されるコンテキスト変数は question_list になります
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     try:
@@ -36,15 +49,27 @@ def index(request):
 #     response = "You're looking at the detail of question %s."
 #     return HttpResponse(response % question)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {
-        'question': question,
-    })
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {
+#         'question': question,
+#     })
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# DetailView 汎用ビューには、 "pk" という名前で URL からプライマリキーをキャプチャして渡すことになっているので
+# デフォルトでは、 DetailView 汎用ビューは <app name>/<model name>_detail.html という名前のテンプレートを使います
+# DetailView には question という変数が自動的に渡されます。なぜなら、 Django モデル (Question) を使用していて、
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -62,4 +87,11 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
+        # reverse関数を使うと、ビュー関数中での URL のハードコードを防げます。
+        # 関数には、制御を渡したいビューの名前と、そのビューに与える URL パターンの位置引数を与えます。
+        # この例では、urls.py で設定した URLconf を使っているので、 
+        # ex: /polls/5/results/
+        # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+        # reverse() を呼ぶと、次のような文字列が返ってきます。
+        # '/polls/5/results/'
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
